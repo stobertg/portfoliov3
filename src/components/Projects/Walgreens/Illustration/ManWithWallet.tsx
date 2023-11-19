460a40c23379a0ad6d4414fc8371bb00e0e4f78b
Tyler Stober <stobertg@gmail.com>
2023-11-19 16:05:07 +0000

    done with documenting the illustration

--- src/components/Projects/Walgreens/Illustration/ManWithWallet.tsx
+++ src/components/Projects/Walgreens/Illustration/ManWithWallet.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { styled } from '@theme'
 import { Blob, Arm, Head, Body } from './Parts'
 
+// For the master container of the illustration animation for Walgreens
+// This shows the man holding the wallet and the money pops out of the wallet
+
 const AnimationWrap = styled('div', {
   display: 'flex',
   justifyContent: 'center',
@@ -10,6 +13,9 @@ const AnimationWrap = styled('div', {
   width: 170,
   height: 170
 })
+
+// For all the content of the illustrations, held in the center of the master container
+// This hols the decorative blob in the background and the man holding the money in the forground
 
 const AnimationContent = styled('div', {
   display: 'flex',
@@ -20,12 +26,18 @@ const AnimationContent = styled('div', {
   transform: 'scale( 0.3 )'
 })
 
-const WalletAnimation = styled('div', {
+// For the container of the main illustration of the man holding the wallet
+// This is filled with animations of the man looking down and then up when the money pops out of the wallet
+
+const Man = styled('div', {
   display: 'inline-flex',
   position: 'relative',
   padding: '0 20px 20px 50px',
   svg: { display: 'flex' }
 })
+
+// For the container of the background elements of the animations
+// This holds the Hhead and the body of the man, seperate from the arm, which is the main focus
 
 const WallletAnimationBase = styled('div', {
   position: 'relative',
@@ -33,15 +45,17 @@ const WallletAnimationBase = styled('div', {
   paddingTop: 143
 })
 
+// ---------- This is the end of declarations ---------- //
+
 export const ManWithWallet = () => {
   return(
 
     <AnimationWrap>
       <AnimationContent>
-        <WalletAnimation>
+        <Man>
           <WallletAnimationBase><Head /><Body /></WallletAnimationBase>
           <Arm />
-        </WalletAnimation>
+        </Man>
 
         <Blob />
       </AnimationContent>
