a77214282d196d3bc5ae5661fb30a890a8450c29
Tyler Stober <stobertg@gmail.com>
2023-11-20 16:36:57 +0000

    update

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -29,7 +29,7 @@ const HeroHeading = styled('div', {
   flexDirection: 'column',
   alignItems: 'center',
   '> *:not(:last-child)': { marginBottom: 8 },
-  '> *:last-child': { maxWidth: '80%' }
+  '> *:last-child': { maxWidth: '90%' }
 })
 
 // For the container of the optional image on the top of the hero section
